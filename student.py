652b18da433d3fa5fa28942b9c072ebc8a781aef
Neka Blair <ourblairfamily@gmail.com>
2024-02-27 22:47:44 -0600

    return name and house in get_students function

--- student.py
+++ student.py
@@ -1,13 +1,12 @@
 def main():
-    name = get_name()
-    house = get_house()
+    # name = get_name()
+    # house = get_house()
     print(f"{name} from {house}")
 
-def get_name():#abstraction
-    return input("Name: ")
-
-def get_house():
-    return input("House: ")
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
 
 if __name__ == "__main__":
     main()
