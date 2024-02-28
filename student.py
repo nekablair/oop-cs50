3632ac61ba2fed96a81942cfd1f238cd04496e01
Neka Blair <ourblairfamily@gmail.com>
2024-02-27 22:49:28 -0600

    destructor name and house

--- student.py
+++ student.py
@@ -1,6 +1,5 @@
 def main():
-    # name = get_name()
-    # house = get_house()
+    name, house = get_student()#unpack sequences of values coming back
     print(f"{name} from {house}")
 
 def get_student():
